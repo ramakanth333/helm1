12e6c22cec7666ddf00584db49a8fb88225e2c76
dev <you@example.com>
2024-03-12 09:24:33 +0000

    all

---
+++ react-frontend/src/services/EmployeeService.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+
+const EMPLOYEE_API_BASE_URL = "http://a7e0e2b72dd474af09523adfddfc92c2-1281631515.us-east-1.elb.amazonaws.com:80/api/v1/employees";
+
+class EmployeeService {
+
+    getEmployees(){
+        return axios.get(EMPLOYEE_API_BASE_URL);
+    }
+
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE_URL, employee);
+    }
+
+    getEmployeeById(employeeId){
+        return axios.get(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+
+    updateEmployee(employee, employeeId){
+        return axios.put(EMPLOYEE_API_BASE_URL + '/' + employeeId, employee);
+    }
+
+    deleteEmployee(employeeId){
+        return axios.delete(EMPLOYEE_API_BASE_URL + '/' + employeeId);
+    }
+}
+
+export default new EmployeeService()
